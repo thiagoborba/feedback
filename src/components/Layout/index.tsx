56c19b608558fc2052f846ef5845b403118321a8
Thiago Bergenthal de Borba <thiago_borba@terceiros.sicredi.com.br>
2022-02-13 03:47:11 -0300

    feat: cria componente layout

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './styles.module.scss'
+
+export const Layout: React.FC = ({ children }) => {
+  return (
+    <div
+      className={styles['container']}
+    >
+      { children }
+    </div>
+  )
+}
