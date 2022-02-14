01a71f6dc278e50401bb4b26c3aac39151fa8db7
Thiago Bergenthal de Borba <thiago_borba@terceiros.sicredi.com.br>
2022-02-14 08:10:19 -0300

    feat: passa app para containers

--- src/containers/app/index.tsx
+++ src/containers/app/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { Feedback } from '../feedback'
+import { FeedbackPage } from '../feedback'
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
+
+const options = {
+  position: positions.TOP_RIGHT,
+  timeout: 5000,
+  offset: '30px',
+  transition: transitions.SCALE
+}
 
 function App() {
   return (
     <main>
-      <Feedback />
+      <AlertProvider template={AlertTemplate} {...options}>
+        <FeedbackPage />
+      </AlertProvider>
     </main>
   );
 }
