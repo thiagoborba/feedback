43e7b1bbebf92fbf365a8f11a485cda0ab3964a0
Thiago Bergenthal de Borba <thiago_borba@terceiros.sicredi.com.br>
2022-02-14 08:09:47 -0300

    feat: cria layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from './styles.module.scss'
 
-export const Layout: React.FC = ({ children }) => {
+export const Layout: React.FC = ({ children, ...props }) => {
   return (
     <div
       className={styles['container']}
+      {...props}
     >
       { children }
     </div>
